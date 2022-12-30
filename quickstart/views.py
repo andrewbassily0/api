4f3be516bb185211a76d0d1237101c68a41b418b
unknown <vitobassily@gmail.com>
2022-12-30 15:28:37 +0200

    api app

---
+++ quickstart/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .serializers import UserSerializer, GroupSerializer
+from rest_framework import viewsets , permissions 
+from django.contrib.auth.models import User, Group
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes =[permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes =[permissions.IsAuthenticated]
+    
